cc37abc956c468f4b69e25779178983b25db4ee0
ahmed mohna <ahmedmohna995@gmail.com>
2022-05-17 21:59:41 +0300

    Initial Commit

---
+++ africa/africaApp.swift
@@ -0,0 +1,17 @@
+//
+//  africaApp.swift
+//  africa
+//
+//  Created by Ahmed Mohna on 17/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct africaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
